32a1506de798054bfa776fa1e64cd809c04ce0e6
gucciwu <gucciwu57@gmail.com>
2016-05-05 14:46:42 +0800

    init

--- convertJson.js
+++ convertJson.js
@@ -4,21 +4,68 @@
 
 //Read json string from file
 function readJsonFile(fileURL){
+    //input: string
+    //output: json
 
     $.getJSON(fileURL,
         function(data){
-            return data;
+            alert(data);
+
+            var dataFormatted;
+            dataFormatted = eval(data);
+
+            return dataFormatted;
         }
     )
 
 }
 
 function convertJson2Array(jsonFileURL){
-    var jsonData, arrayData;
+    var jsonData;
 
     jsonData = readJsonFile(jsonFileURL);
-    arrayData = parseJSON(jsonData);
+    alert(jsonData);
+    return $.parseJSON(jsonData);
 
+}
+
+function buildTimeSharingX(jsonFileURL){
+    //build xAxis data for time sharing chart
+    //input: array
+    //output: array
+
+    var jsonData, stockData;
+    stockData = new Array();
+
+    $.getJSON(jsonFileURL,
+        function(data){
+
+            jsonData = eval(data);
+
+            for(var i=0;i<jsonData.length-1;i++){
+                stockData.push(jsonData[i].time);
+            }
+            return stockData;
+        }
+    );
+}
+
+function buildTimeSharingY(jsonFileURL){
+
+    var jsonData, stockData;
+    stockData = new Array();
+
+    $.getJSON(jsonFileURL,
+        function(data){
+
+            jsonData = eval(data);
+
+            for(var i=0;i<jsonData.length-1;i++){
+                stockData.push(jsonData[i].price);
+            }
+            return stockData;
+        }
+    );
 }
 
 
